d340990bcc4f6b8a0f1ac6dca93df2d828b3ac02
Aya Bitar <aya.b@gso.schule.koeln>
2022-03-31 08:47:11 +0200

    Testcommit

--- WuerfelspielTests/WuerfelTests.cs
+++ WuerfelspielTests/WuerfelTests.cs
@@ -13,6 +13,8 @@ namespace WuerfelspielTests
             // Dieser Test ist hier, um eine GitHub-Funktion zu testen. 
             // Einfach löschen :)
             Assert.IsTrue(true);// is tatsächlich true :O
+
+            //irgendwas
         }
     }
 }
